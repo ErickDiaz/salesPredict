0495108d2dcfdc4efde0657605c07e73614ecfe0
erick <erick@autoweb.com>
2016-01-08 12:25:40 -0600

    prediccion completada falta ajustar el modelo

--- functions.R
+++ functions.R
@@ -9,6 +9,26 @@ readCSV <- function(){
   return(data)  
 }
 
+summaryModel <- function( s ){
+  rse <- s$sigma
+  df <- s$df[2]
+  mse <- s$r.squared
+  pa <- mse *100
+  
+  print(paste("degrees of freedom",df))
+  print(paste("Residual standard error: ",rse))  
+  print(paste("Multiple R squared error : ",mse))
+  print( paste("Prediction accuracy :", pa , "%", collapse=" "))
+  
+  return(mse)
+}
+
+plotDataFrame <- function (df) {
+  plot(x=df$date, y=df$y,col='black',type="l" )        
+  points(x=df$date , y=df$trend, col='red', type='l', lwd=2)  
+}
+
+
 predictSales <- function(){
   library(sqldf)
   options(digits=8)
@@ -16,7 +36,21 @@ predictSales <- function(){
   
   raw_data <- readCSV()
   
-  df <- sqldf( "select QTR_ID, MONTH_ID, YEAR_ID, PRODUCTLINEID, sum(SALES) from raw_data group by 1,2,3,4" )
+  df <- sqldf( "select QTR_ID as x1, MONTH_ID as x2, YEAR_ID as x3, PRODUCTLINEID as x4, sum(SALES) as y 
+               from raw_data 
+               group by 1,2,3,4
+               order by x1,x2,x3" )
+  
+  
+  
+  lm <- lm(y ~  x1 + x2 + x3 + x4, data = df)
+  summaryModel(summary(lm))
+  
+  res <- predict(lm,df)  
+  df[, "trend"] <- res
   
   print(df)
+  
+  
 }
+
